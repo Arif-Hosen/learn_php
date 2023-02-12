de73a3a0a39411be039b143710cde69266e3e50b
arif_hosen <arif.hosen3888@gmail.com>
2023-02-12 14:50:03 +0600

    auto loading

--- oop/autoload/car/Car.php
+++ oop/autoload/car/Car.php
@@ -3,21 +3,24 @@ use Car\BMW;
 use Car\Mercedez;
 
 /*
-* auto load - sometimes we need multiple file's class in a file, here we use autoloading. 
-* psr- php standard recommendation - is set of standards which ensure working with php easier. ex- psr1- coding standard, psr7- http request standard, psr4- autoloading standard.
-* psr4 - provides the ability  to include classes based on their file path. like, you define BMW, Mercedex classes in different file and their namespace is Car. To load those file we need include 2 files for twice. using psr4 we can include file with autoloading.
+* auto load - Sometimes we need multiple files of classes in a file, here we can use autoload.
 
-step1 - run composer init to top directory
-step2 - add "autoload" property in composer.json file, add "psr-4" property - it's property is namespaces of classes and value is file directory (where auto load will find the namespaces).
+* PSR- PHP Standard Recommendation - is a set of standards that ensure working with PHP is easier. Example: PSR1- Coding standard, PSR7- HTTP request standard, PSR4- Autoloading standard.
+
+* PSR4 - Provides the ability to include classes based on their file path. Like, you define BMW, and Mercedez classes in a different file and their namespace is Car. To load those files we need to include 2 files twice. Using PSR4 we can include files with autoloading. So, we don't need include files of classes again again.
+
+Steps of using auto load-
+Step 1: If you have installed composer. Run 'composer init' to the top directory of your project folder.
+Step 2: Add the "autoload" property in the composer.json file, and add the "psr-4" property - its property is namespaces of classes and the value is file directory (where the auto load will find the namespaces).
 ex--
 "autoload": {
         "psr-4": {
             "Car\\": "oop/autoload/car/"
         }
     },
-step3 - run composer dump-autoload
-step4 - include the vendor/autoload.php file where you need those classes.
-* note-  when you include vendor/autoload.php, becareful about your current directory. like - vendor/autoload.php is 3 step upper directory from your current directory, here you use- (include_once "../../../vendor/autoload.php");
+Step 3: Run "composer dump-autoload"
+Step 4: Include the "vendor/autoload.php" file where you need those classes.
+*Note:  When you include vendor/autoload.php, be careful about your current directory. For - "vendor/autoload.php" is in 3 steps upper directory from your current directory, here you use- (include_once "../../../vendor/autoload.php").
 */ 
 
 include_once "../../../vendor/autoload.php";
